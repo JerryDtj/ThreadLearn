bb5eb246a60fab800a195289d4f4cccc4e95715f
邓天骄 <jiao_snow@126.com>
2018-03-05 20:48:42 +0800

    线程的优先级，直接继承无效，改变当前启动线程优先级有效

--- src/test/java/PriorityTest.java
+++ src/test/java/PriorityTest.java
@@ -12,10 +12,10 @@ public class PriorityTest {
         t1.setPriority(Thread.MAX_PRIORITY);
         Thread t3 = new Thread(new ThreadB());
         t3.start();
-        Thread t4 = new Thread(new ThreadC());
+        Thread t4 = new Thread(new ThreadC());//直接继承是无法改变线程的优先级的
         t4.start();
 
-        Thread.currentThread().setPriority(6);
+        Thread.currentThread().setPriority(6);//改变当前启动线程的优先级才有效果
         Thread t5 = new Thread(new ThreadA());
         t5.start();
         Thread t6 = new Thread(new ThreadB());
