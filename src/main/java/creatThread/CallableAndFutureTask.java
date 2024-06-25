5c70a0ff12dc0bcfe60dc406fe5247c2f551d7a0
jerry <jiao_snow@126.com>
2024-06-25 17:39:41 +0800

    add future init

--- src/main/java/creatThread/CallableAndFutureTask.java
+++ src/main/java/creatThread/CallableAndFutureTask.java
@@ -5,9 +5,9 @@ import java.util.concurrent.Callable;
 public class CallableAndFutureTask implements Callable {
 
 
-    public Object call() throws Exception {
+    public String call() throws Exception {
         System.out.println("实现线程的第三种方式：Callable");
         System.out.println(111);
-        return null;
+        return "123";
     }
 }
--- src/test/java/creatThread/CreatThreadTest.java
+++ src/test/java/creatThread/CreatThreadTest.java
@@ -1,10 +1,9 @@
 package creatThread;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class CreatThreadTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //继承多线程实现
         ThreadExtend threadExtend = new ThreadExtend();
         threadExtend.start();
@@ -13,8 +12,25 @@ public class CreatThreadTest {
         threadImpl.start();
         //通过callable和futureTask实现
         Callable callable = new CallableAndFutureTask();
-        FutureTask futureTask = new FutureTask(callable);
+        FutureTask<String> futureTask = new FutureTask(callable);
         Thread callableThread = new Thread(futureTask);
         callableThread.start();
+
+        Thread futureThread = new Thread(futureTask);
+        futureThread.start();
+        String s = (String) futureTask.get();
+        System.out.println(s);
+        s = null;
+
+        ExecutorService executors = Executors.newSingleThreadExecutor();
+        executors.submit(futureThread);
+        System.out.println("2:"+futureTask.get());
+        s = null;
+
+        Future<?> f = executors.submit(callable);
+        System.out.println("3:"+f.get());
+
+        executors.shutdown();
+
     }
 }
